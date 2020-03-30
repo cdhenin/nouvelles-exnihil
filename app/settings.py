a4cdd5dacd8f72a3080cd5ac4f07454077b0dcea
Claire Dhénin <claire.dhenin@gmail.com>
2020-03-30 09:10:57 +0200

    add presentation and fix whitespace

--- app/settings.py
+++ app/settings.py
@@ -140,5 +140,6 @@ TINYMCE_DEFAULT_CONFIG = {
   'width': '100%',
   'menubar': False,
   'toolbar': 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-  'content_css': '/static/codepen.css'
+  # 'content_css': '/static/codepen.css',
+  'valid_elements' : '-p'
 }
